fc16a9657703a1bbca8185b7f4ba0c1849da0f25
davidegarigilo <s248377@studenti.polito.it>
2022-07-09 14:02:50 +0000

    correct path for the output of the grid-search

--- script_test_parameters.sh
+++ script_test_parameters.sh
@@ -7,7 +7,7 @@ gamma=(0.003 0.03 0.3)
 bs=(64 128 256)
 
 source=1
-target=3
+target=2
 aggr=avgpool
 if [ $aggr = "avgpool" ];then
   g=0.003
@@ -16,21 +16,24 @@ else
   g=0.03
   b=128
 fi
+
+path='../drive/MyDrive/MLDL_2022/project/parametersD'$source'-D'$target''$aggr'_N_N_Y.txt'
+
 for alpha in "${alphas[@]}"; do
-  echo "Alpha: $alpha" >> '../drive/MyDrive/MLDL_2022/project/parameters_D'$source'-D'$target'_'$aggregation'.txt'
+  echo "Alpha: $alpha" >> "$path"
     #for b0 in "${beta0[@]}"; do
-      #echo -e "\tBeta0: $b0" >> ../drive/MyDrive/MLDL_2022/project/parameters_D1-D2_avgpool.txt
+      #echo -e "\tBeta0: $b0" >> $path
         #for b1 in "${beta1[@]}"; do
-          #echo -e "\t\tBeta1: $b1" >> ../drive/MyDrive/MLDL_2022/project/parameters_D1-D2_avgpool.txt
+          #echo -e "\t\tBeta1: $b1" >> $path
             for b2 in "${beta2[@]}"; do
-              echo -e "\tBeta2: $b2" >> '../drive/MyDrive/MLDL_2022/project/parameters_D'$source'-D'$target'_'$aggregation'.txt'
+              echo -e "\tBeta2: $b2" >> $path
                 for g in "${gamma[@]}"; do
-                  echo -e "\t\tGamma: $g" >> '../drive/MyDrive/MLDL_2022/project/parameters_D'$source'-D'$target'_'$aggregation'.txt'
+                  echo -e "\t\tGamma: $g" >> $path
                     for b in "${bs[@]}"; do
-                      echo -e "\t\t\tBatch: $b" >> '../drive/MyDrive/MLDL_2022/project/parameters_D'$source'-D'$target'_'$aggregation'.txt'
-                        rm -rf model
-                        ./script_test_ta3n.sh true true $source $target $aggr 'N N Y'\
-                        $alpha -1 -1 $b2 $g $b
+                      echo -e "\t\t\tBatch: $b" >> $path
+                      rm -rf model
+                      ./script_test_ta3n.sh true true $source $target $aggr 'N N Y'\
+                      $alpha -1 -1 $b2 $g $b
                     done
                 done
             done
